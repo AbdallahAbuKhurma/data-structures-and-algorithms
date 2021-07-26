585df6647e135af79ba7694c7f528b699409d4b8
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-07-26 15:47:21 +0300

    challenge 17 done

--- 401-code-challenges/trees/binaryTrees.js
+++ 401-code-challenges/trees/binaryTrees.js
@@ -1,101 +1,130 @@
 'use strict';
 
 class BinaryTree {
-  constructor(root){
+  constructor(root) {
     this.root = root;
   }
 
   preOrder() {
     try {
-      if(!this.root) {
+      if (!this.root) {
         throw new Error('empty tree');
       }
-      const result = [];
+      const queue = [];
       const _traverse = (node) => {
-        result.push(node.value);
-        if(node.left) {
+        queue.push(node.value);
+        if (node.left) {
           _traverse(node.left);
         }
-        if(node.right) {
+        if (node.right) {
           _traverse(node.right);
         }
       };
       _traverse(this.root);
-      return result;
+      return queue;
     } catch (error) {
-      console.error({message: error.message});
+      console.error({ message: error.message });
     }
   }
 
   postOrder() {
     try {
-      if(!this.root) {
+      if (!this.root) {
         throw new Error('empty tree');
       }
-      const result = [];
+      const queue = [];
       const _traverse = (node) => {
-        if(node.left) {
+        if (node.left) {
           _traverse(node.left);
         }
-        if(node.right) {
+        if (node.right) {
           _traverse(node.right);
         }
-        result.push(node.value);
+        queue.push(node.value);
       };
       _traverse(this.root);
-      return result;
+      return queue;
     } catch (error) {
-      console.error({message: error.message});
+      console.error({ message: error.message });
     }
   }
 
   inOrder() {
     try {
-      if(!this.root) {
+      if (!this.root) {
         throw new Error('empty tree');
       }
-      const result = [];
+      const queue = [];
       const _traverse = (node) => {
-        if(node.left) {
+        if (node.left) {
           _traverse(node.left);
         }
-        result.push(node.value);
-        if(node.right) {
+        queue.push(node.value);
+        if (node.right) {
           _traverse(node.right);
         }
       };
       _traverse(this.root);
-      return result;
+      return queue;
     } catch (error) {
-      console.error({message: error.message});
+      console.error({ message: error.message });
     }
   }
 
-  getMaxValue(){
+  getMaxValue() {
     try {
-      if(!this.root){
-        return ('empty tree');
+      if (!this.root) {
+        return 'empty tree';
       }
       let maxValue = this.root.value;
       const _traverse = (node) => {
-        if(node.value >= maxValue){
+        if (node.value >= maxValue) {
           maxValue = node.value;
         }
-        if(node.right){
+        if (node.right) {
           _traverse(node.right);
         }
-        if(node.left){
+        if (node.left) {
           _traverse(node.left);
         }
       };
       _traverse(this.root);
       return maxValue;
+    } catch (error) {
+      console.error({ message: error.message });
+    }
+  }
+
+  breadthFirst(tree) {
+    try {
+      if (!this.root) {
+        return 'empty tree';
+      }
+      let queue = [];
+      let results = [];
+      tree = this.root;
+      queue.push(tree);
+
+      const _traverse = (node) => {
+        while(queue.length > 0){
+          node = queue[0];
+          results.push(node.value);
+          if(node.left !== null){
+            queue.push(node.left);
+          }
+          if(node.right !== null){
+            queue.push(node.right);
+          }
+          queue.shift();
+        }
+      };
+      _traverse(tree);
+      return results;
 
     } catch (error) {
       console.error({message: error.message});
     }
   }
-
 }
 
 module.exports = BinaryTree;
